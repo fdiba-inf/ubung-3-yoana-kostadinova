f4f03c056e332129d4d28d45ee1fbbf4aa55f789
yoana11 <ani.kostadinova2001@gmail.com>
2020-10-14 14:26:10 +0000

    a lot

--- src/exercise3/NumberCounter.java
+++ src/exercise3/NumberCounter.java
@@ -3,30 +3,32 @@ import java.util.Scanner;
 public class NumberCounter {
   public static void main (String [] args) {
 
-    Scanner input = new Scanner (System.in);
+    Scanner input = new Scanner(System.in);
   
-    int number = input.nextInt ();
+    int number = input.nextInt();
     int countp = 0;
     int countn = 0;
-    int sum = 0;
+    double sum = 0;
 
     while (number != 0 ) {
     if (number > 0) {
       countp++;
       sum = sum + number;
+      number = input.nextInt();
+
     }
     else {
       countn++;
       sum = sum + number;
-    }    
-    int average = sum/ (countp + countn);
-
-    System.out.println ("Positive numbers: " + countp);
-    System.out.println ("Negatve numbers: " + countn);
-    System.out.println ("Sum: " + sum);
-    System.out.println ("Average: " + average);
-
+      number = input.nextInt();
+    }        
     }
+    double average = sum/ (countp + countn);
+
+    System.out.println("Positive numbers: "+countp);
+    System.out.println("Negatve numbers: "+countn);
+    System.out.println("Sum: "+sum);
+    System.out.println("Average: "+average);
 
     
   }
